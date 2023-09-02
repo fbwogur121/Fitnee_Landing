a67d43d4a0429eea5ea405ff0606cf634be5a743
Choi Seho <kindseho@naver.com>
2023-09-03 03:41:45 +0900

    feat: 로고 이미지 추가

--- landing-page/app/promotion/page.js
+++ landing-page/app/promotion/page.js
@@ -1,6 +1,9 @@
-export default function Promotion(){
-    return(
-        <h1>사전예약 페이지 입니다.</h1>
-    )
-
+export default function Promotion() {
+  return (
+    <div className="promotion">
+      <div className="title"></div>
+      <div className="title-sub"></div>
+      <textarea className="email"></textarea>
+    </div>
+  );
 }
