8dd360448d7ad15030d3615bc4e4dfc0f90bf892
joohee <2170095@ewhain.net>
2023-09-04 15:16:23 +0900

    폰트 적용

--- landing-page/app/page.js
+++ landing-page/app/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 export default function Home() {
-    return (
-        <div>
-            <div className="home-content"></div>
-            <Link href='/aboutus'>aboutus로 가기</Link>
-            <Link href='/promotion'>promotion로 가기</Link>
-        </div>
-    )
+  return (
+    <div>
+      <div className="home-content"></div>
+      <Link href="/aboutus">aboutus로 가기</Link>
+      <Link href="/promotion">promotion로 가기</Link>
+    </div>
+  );
 }
