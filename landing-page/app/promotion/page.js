6320582db290b2067b8167f7fbe528a1bf1224ce
Choi Seho <kindseho@naver.com>
2023-09-05 21:26:24 +0900

    feat: promotion desktop, tablet 완료

--- landing-page/app/promotion/page.js
+++ landing-page/app/promotion/page.js
@@ -1,13 +1,17 @@
 "use client";
+import Image from "next/image";
 import { useState } from "react";
+import Checked from "../../public/checked.svg";
+import NonChecked from "../../public/non-checked.svg";
 
 export default function Promotion() {
   const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
   const [checkbox1, setCheckbox1] = useState(false);
   const [checkbox2, setCheckbox2] = useState(false);
 
   // email 입력 값이 있고, checkbox1 및 checkbox2가 모두 선택되어야 버튼이 활성화됩니다.
-  const isSubmitEnabled = email && checkbox1 && checkbox2;
+  const isSubmitEnabled = email && checkbox1 && checkbox2 && tel;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,49 +34,70 @@ export default function Promotion() {
 
   return (
     <div className="promotion">
-      <div className="title">fitnee 사전예약</div>
-      <div className="title-sub">
+      <div className="promotion-title">fitnee 사전예약</div>
+      <div className="promotion-sub-title">
         런칭이 시작되면 작성해주신 메일 주소로 알림을 보내드려요
       </div>
-      <form onSubmit={handleSubmit}>
-        <p>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-wrapper">
           <input
             type="email"
             name="email"
+            className="email-input" // 클래스 추가
             placeholder="fitnee@gmail.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-        </p>
-        <div>
+        </div>
+        <div className="input-wrapper">
           <input
             type="tel"
             name="tel"
+            className="tel-input" // 클래스 추가
             placeholder="01012345678"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            value={tel}
+            onChange={(e) => setTel(e.target.value)}
           />
         </div>
-        <div>
-          <input
-            type="checkbox"
-            name="checkbox1"
-            checked={checkbox1}
-            onChange={(e) => setCheckbox1(e.target.checked)}
-          />
-          <div>개인정보 수집 및 목적</div>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            name="checkbox2"
-            checked={checkbox2}
-            onChange={(e) => setCheckbox2(e.target.checked)}
-          />
-          <div>마케팅 정보 수신 동의</div>
+        <div className="checkbox-wrapper">
+          <div className="checkbox-container">
+            <div className="checkbox" onClick={() => setCheckbox1(!checkbox1)}>
+              {checkbox1 ? (
+                <Image src={Checked} width={26} height={26} alt="체크 이미지" />
+              ) : (
+                <Image src={NonChecked} width={26} height={26} />
+              )}
+            </div>
+            <label className="checkbox-label">개인정보 수집 및 목적</label>
+            <div className="essential-box">
+              <div className="essential">필수</div>
+            </div>
+          </div>
+          <div className="checkbox-container">
+            <div className="checkbox" onClick={() => setCheckbox2(!checkbox2)}>
+              {checkbox2 ? (
+                <Image src={Checked} width={26} height={26} alt="체크 이미지" />
+              ) : (
+                <Image src={NonChecked} width={26} height={26} />
+              )}
+            </div>
+            <label className="checkbox-label">마케팅 정보 수신 동의</label>
+            <div className="essential-box">
+              <div className="essential">필수</div>
+            </div>
+          </div>
         </div>
         <p>
-          <input type="submit" value="신청하기" disabled={!isSubmitEnabled} />
+          <input
+            type="submit"
+            className={`submit-button ${isSubmitEnabled ? "" : "disabled"}`} // 클래스 추가
+            value="신청하기"
+            style={{
+              color: isSubmitEnabled ? "white" : "black",
+              background: isSubmitEnabled ? "#8457F1" : "#E0E2EE",
+            }}
+            disabled={!isSubmitEnabled}
+          />
         </p>
       </form>
     </div>
