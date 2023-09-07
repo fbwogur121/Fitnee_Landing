98d8f02c31f969a2dd47ecd5c5983dcc9560f999
Choi Seho <kindseho@naver.com>
2023-09-08 02:31:31 +0900

    style: 약관 닫기버튼 스타일 수정

--- landing-page/component/modal.js
+++ landing-page/component/modal.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Close from "../public/close-circle.svg";
+
 export default function Modal({ setModalOpen }) {
   // 모달 끄기
   const closeModal = () => {
@@ -6,15 +9,16 @@ export default function Modal({ setModalOpen }) {
 
   return (
     <div className="modal-container">
-      <button
+      <div
         className="modal-close"
         style={{
           position: "sticky",
         }}
         onClick={closeModal}
       >
-        X
-      </button>
+        <Image src={Close} width={24} height={24} alt="닫기 이미지" />
+      </div>
+
       <div
         className="modal-terms"
         style={{ overflow: "auto", whiteSpace: "pre-wrap" }}
